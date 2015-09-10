3b95709339b0c2d629d4550feed3eef72c212911
kyf <kyf456@163.com>
2015-09-10 17:03:39 +0800

    add admin

--- admin.go
+++ admin.go
@@ -13,6 +13,10 @@ func init() {
 			"method": METHOD_POST,
 			"handler": func(r *http.Request) string {
 				r.ParseForm()
+
+				circle := r.Form.Get("circle")
+				pass := r.Form.Get("password")
+
 				result := make([]string, 0)
 				for k, v := range r.Form {
 					result = append(result, fmt.Sprintf("%v=%v", k, v))
